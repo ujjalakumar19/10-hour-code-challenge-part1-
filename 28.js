27687529a3ded3953c76b912e2cc371711acfce7
ujjala kumari <134491878+ujjalakumar19@users.noreply.github.com>
2023-06-14 00:38:43 -0700

    Add files via upload

---
+++ 28.js
@@ -0,0 +1,21 @@
+// for loop in array
+
+let fruits = ["apple","mango","grapes",]
+
+// for(i=0;i<=9;i++){
+//     console.log(i);
+// }
+
+//length properties <= ye properties array ki length batati hai
+
+// console.log(fruits.length);
+// console.log(fruits[1]);
+
+// last element ko jana kay liye -1 ka use karta HTMLMediaElement
+
+// console.log(fruits[fruits.length-1]);
+let fruits2 = [];
+for(let i=0;i<=fruits.length-1;i++){
+    console.log(fruits[i]. toUpperCase());
+}
+console.log(fruits2);
